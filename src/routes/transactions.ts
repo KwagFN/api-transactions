8d769c6fc5c779e903d8c31d448d85f06edacb66
TheFNetto <franciscojdsnetto@gmail.com>
2024-09-19 18:10:48 -0300

    feat: add createTransaction route

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,12 +1,27 @@
 import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
+import { randomUUID } from 'node:crypto'
 import { SqliteHelper } from '../database'
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/hello', async () => {
-    const transactions = await SqliteHelper('transactions')
-      .where('amount', 1000)
-      .select('*')
+  app.post('/', async (request, reply) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
 
-    return transactions
+    const { title, amount, type } = createTransactionBodySchema.parse(
+      request.body,
+    )
+
+    await SqliteHelper('transactions').insert({
+      id: randomUUID(),
+      title: title,
+      amount: type == 'credit' ? amount : amount * -1,
+      session_id: randomUUID(),
+    })
+
+    return reply.status(201).send('Transaction created successfuly.')
   })
 }
