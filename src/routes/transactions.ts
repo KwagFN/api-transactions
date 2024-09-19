6ccbdd4399dd8e9d32c11e6bcb40a4f1a0f2bf38
TheFNetto <franciscojdsnetto@gmail.com>
2024-09-19 18:31:10 -0300

    feat: add get all transactions and get transaction by id route

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,6 +4,30 @@ import { randomUUID } from 'node:crypto'
 import { SqliteHelper } from '../database'
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const transactions = await SqliteHelper('transactions').select('*')
+
+    return {
+      transactions: transactions,
+    }
+  })
+
+  app.get('/:id', async (request) => {
+    const getTransactionById = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getTransactionById.parse(request.params)
+
+    const transaction = await SqliteHelper('transactions')
+      .where('id', id)
+      .first()
+
+    return {
+      transaction,
+    }
+  })
+
   app.post('/', async (request, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
